d4eaf5d3f4fe7235ba9422e24cef1e116a0bc7c2
KikoBM <kikobm7@uma.es>
2021-05-13 00:50:08 +0200

    acciones en mis eventos
    ver evento y borrar evento funciona; hay que cambiar algunas cosas de editar evento para que funcione correctamente

---
+++ EventosGO/src/java/eventosgowebapp/servlet/ServletEventoVer.java
@@ -0,0 +1,100 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eventosgowebapp.servlet;
+
+import eventosgowebapp.dao.EventoFacade;
+import eventosgowebapp.entity.Evento;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.ejb.EJB;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Kiko BM
+ */
+@WebServlet(name = "ServletEventoVer", urlPatterns = {"/ServletEventoVer"})
+public class ServletEventoVer extends HttpServlet {
+
+    @EJB
+    private EventoFacade eventoFacade;
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String strTo = "verEvento.jsp";
+        
+        String id = request.getParameter("eventoid");
+        
+        String accion;
+        accion = (String)request.getParameter("accion");
+        
+        if(accion == null){
+            strTo = "verEvento.jsp";
+        }else if (accion.equalsIgnoreCase("editar")){
+            strTo = "editarEvento.jsp";
+        }
+        
+        Evento elevento = this.eventoFacade.find(new Integer(id));
+        request.setAttribute("evento", elevento);
+        
+        RequestDispatcher rd = request.getRequestDispatcher(strTo);
+        rd.forward(request, response);
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
